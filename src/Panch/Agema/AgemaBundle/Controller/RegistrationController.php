678dd2c1ce186bfc358e528313087ff6ed84d587
Vadym Panchenko <panchenko.vadym@gmail.com>
2015-04-06 02:18:26 +0300

    Fix

--- src/Panch/Agema/AgemaBundle/Controller/RegistrationController.php
+++ src/Panch/Agema/AgemaBundle/Controller/RegistrationController.php
@@ -22,8 +22,10 @@ class RegistrationController extends BaseController
     {
         /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
         $formFactory = $this->get('fos_user.registration.form.factory');
+
         /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
         $userManager = $this->get('fos_user.user_manager');
+
         /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
         $dispatcher = $this->get('event_dispatcher');
         $user = $userManager->createUser();
@@ -31,14 +33,17 @@ class RegistrationController extends BaseController
         $user->setRoles(array(
             'user_roles' => 'ROLE_DEFAULT',
         ));
+
         $event = new GetResponseUserEvent($user, $request);
         $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
         if (null !== $event->getResponse()) {
             return $event->getResponse();
         }
+
         $form = $formFactory->createForm();
         $form->setData($user);
         $form->handleRequest($request);
+
         if ($form->isValid()) {
             $event = new FormEvent($form, $request);
             $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
@@ -48,8 +53,10 @@ class RegistrationController extends BaseController
                 $response = new RedirectResponse($url);
             }
             $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
             return $response;
         }
+
         return $this->render('@PanchAgema/FOSUserBundle/views/Registration/register.html.twig', array(
             'page_title' => 'User Registration',
             'form' => $form->createView(),
